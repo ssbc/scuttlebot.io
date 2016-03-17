48dd38e9b7b5298bdac2406eadb91e13ba6494bb
Paul Frazee <pfrazee@gmail.com>
2016-03-16 20:56:49 -0500

    cut out a lot of the footer, for now

--- tmpl/footer.part.js
+++ tmpl/footer.part.js
@@ -7,19 +7,6 @@ function item (current, path, label) {
 
 module.exports = (c) => `<div class="footer">
   <ul class="footer-section">
-    <li>Guides</li>
-    ${item(c, '/guides/lets-write-a-todo-list-app/intro.html', 'Let\'s Write: a Todo-List app')}
-    ${item(c, '/guides/lets-write-a-private-messaging-app/intro.html', 'Let\'s Write: a Private Messaging app')}
-    ${item(c, '/guides/lets-write-a-file-sharing-app/intro.html', 'Let\'s Write: a File-Sharing app')}
-    ${item(c, '/guides/how-to-use-pull-streams/intro.html', 'How to use Pull Streams')}
-  </ul>
-  <ul class="footer-section">
-    <li>Applications</li>
-    <li><a href="https://github.com/ssbc/patchwork" title="Patchwork">Patchwork</a>
-    <li><a href="https://github.com/ssbc/ssb-blessed-dashboard" title="CLI Dashboard">CLI Dashboard</a>
-    <li><a href="https://github.com/ssbc/ssb-example-pm" title="CLI Private Msg">CLI Private Msg</a>
-  </ul>
-  <ul class="footer-section">
     <li>Community</li>
     <li><p>#scuttlebutt on freenode</p></li>
     <li><a href="https://github.com/ssbc">GitHub Org</a></li>
