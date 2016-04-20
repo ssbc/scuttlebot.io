5a4efee346b14e13e01e1b9cd46b3b7a9bdc95ba
Paul Frazee <pfrazee@gmail.com>
2016-04-20 10:39:24 -0500

    remove the cta entirely from the homepage

--- tmpl/index.html.js
+++ tmpl/index.html.js
@@ -10,10 +10,10 @@ module.exports = () => page({
     <div class="hero big">
       <img src="/img/hermies-256.png">
       <h1>Scuttlebot<br><small>a peer-to-peer messenger<br>for people tired of cloud software</small></h1>
-      <p>
-        ${''/*<a class="cta" href="javascript:alert('TODO: This will popup to a youtube video demoing sbot install and usage, and then patchwork')">&#9654; See it in action</a>*/}
+      ${''/*<p>
+        <a class="cta" href="javascript:alert('TODO: This will popup to a youtube video demoing sbot install and usage, and then patchwork')">&#9654; See it in action</a>
         <a class="cta dark" href="/docs/basics/install-the-database.html"><small>&#11015;</small> Download now</a>
-      </p>
+      </p>*/}
       <p class="description">
         Scuttlebot is an open source <strong>peer-to-peer log store</strong> used as a database, identity provider, and messaging system.
         It features global replication, file-syncronization, and end-to-end encryption.<br>
