9c345e328af8fa7db506298bd2606429fc377fe7
David Winn <q@qypea.com>
2019-03-14 06:16:04 -0700

    Add app description for ssb-client-cli

--- tmpl/apps/index.html.js
+++ tmpl/apps/index.html.js
@@ -12,6 +12,7 @@ module.exports = () => page({
     { id: 'ferment', title: 'Ferment', desc: 'Peer-to-peer audio publishing and streaming.', url: 'http://ferment.audio' },
     { id: 'ssb-cli-dashboard', title: 'CLI Dashboard', desc: 'Browse the database state.', url: 'https://github.com/ssbc/ssb-cli-dashboard' },
     { id: 'ssb-simple-whois', title: 'CLI Whois', desc: 'Very simple petname->pubkey lookup.', url: 'https://github.com/ssbc/ssb-simple-whois' },
+    { id: 'ssb-client-cli', title: 'ssb-client-cli', desc: 'Command Line interface to do basic things with SSB', url: 'https://github.com/qypea/ssb-client-cli' },
     { id: 'git-ssb', title: 'git ssb', desc: 'git repos and issue tracking', url: 'https://git.scuttlebot.io/%25n92DiQh7ietE%2BR%2BX%2FI403LQoyf2DtR3WQfCkDKlheQU%3D.sha256' },
     { id: 'dnssb', title: 'dnssb', desc: 'publish, distribute and query dns records', url: 'https://git.scuttlebot.io/%25aVOBlkoiDbK99ROZPIaiiDk%2B4q2P4%2BG7MGul4UxkBBM%3D.sha256' },
     { id: 'add-new', title: 'Add an application', url: 'https://github.com/ssbc/scuttlebot.io/blob/master/add-an-application.md' }
